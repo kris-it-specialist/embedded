0254340276419374af511cbb6e21ab25580dacfe
klorenz <krzysztof_lorenz@wp.pl>
2021-06-04 20:19:35 +0200

    rozne znaczki

---
+++ podstawy/Wyswietl_napis.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Wyswietl\n bardzo\n dlugi\n napis\n");
+}
---
+++ podstawy/Wyswietl_rozne_znaki.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+int main()
+{
+	printf("Bardzo dziwne znaczki\n  \\ %% & / $  \n");
+}
